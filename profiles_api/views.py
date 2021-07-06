c3b215c905f279a90a2ba5f15c8020734b89bc2d
Rohit Verma <verma.rohit.in@gmail.com>
2021-07-06 14:26:40 +0530

    create user api, get user api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,8 @@ from rest_framework import status, viewsets
 
 from profiles_api import serializers
 
+from profiles_api import models
+
 class HelloApiView(APIView):
     """Test API View""" 
     serializer_class = serializers.HelloSerializer
@@ -94,3 +96,8 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle removing an object"""
         return Response({'http_method':'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all() 
