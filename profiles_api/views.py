8668392d43d1df6266c69b0be11f2803f1e89f20
Rohit Verma <verma.rohit.in@gmail.com>
2021-07-10 14:36:18 +0530

    prevent unauthorized access to feed list

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -5,7 +5,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 from profiles_api import serializers
 
@@ -125,7 +125,7 @@ class UserProfileFeedViewSet(viewsets.ModelViewSet):
     queryset = models.ProfileFeedItem.objects.all()
     permission_classes = (
         permissions.UpdateOwnStatus,
-        IsAuthenticatedOrReadOnly
+        IsAuthenticated
     )
 
     def perform_create(self, serializer):
